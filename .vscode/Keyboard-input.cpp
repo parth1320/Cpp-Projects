ea41f378d405a1563f292b1c67babef67c139178
PARTH_KAKADIYA <51327821+parth1320@users.noreply.github.com>
2024-06-01 20:42:54 +0200

    Input from keyboard

---
+++ .vscode/Keyboard-input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter the temperature value in feranheit: ";
+    double value = 56;
+    // cin >> value;
+    cout << (value - 32) * (5 / 9);
+
+    // int x;
+    // cin >> x;
+    // cout << x;
+    // return 0;
+}
